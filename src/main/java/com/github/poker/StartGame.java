88c5716eb09c00883f280ed2d082765a7af28533
James Baker <bakerjames1995@gmail.com>
2017-03-12 13:37:32 +0000

    Rename App.java to StartGame.java

---
+++ src/main/java/com/github/poker/StartGame.java
@@ -0,0 +1,13 @@
+package com.github.poker;
+
+/**
+ * Hello world!
+ *
+ */
+public class StartGame
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Starting Game...");
+    }
+}
