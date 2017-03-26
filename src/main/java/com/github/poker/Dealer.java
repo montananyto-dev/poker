b47a627d49bdfa3a39f8543145603149a8d636be
Kizenku <bakerjames1995@gmail.com>
2017-03-26 14:17:25 +0100

    Cleanup Dealer.java

--- src/main/java/com/github/poker/Dealer.java
+++ src/main/java/com/github/poker/Dealer.java
@@ -2,33 +2,35 @@ package com.github.poker;
 
 import java.util.ArrayList;
 
-public class Dealer{
+public class Dealer {
 
   private String name;
-  private static ArrayList<Player> player;
+  private ArrayList<Player> players;
   private static Dealer dealer;
 
-private Dealer(){}
+  private Dealer() {}
 
-public static Dealer getInstance(){
-  if(dealer == null){
-    dealer = new Dealer();
-    System.out.println("Dealer created");
-  }
-  return dealer;
-}
-
-public void setName(String name){
-  this.name = name;
+  public static Dealer getInstance() {
+    if(dealer == null){
+      dealer = new Dealer();
+      System.out.println("Dealer created");
+    }
+    return dealer;
   }
 
-public String getName(){
-  return name;
+  public void setName(String name) {
+    this.name = name;
   }
-public void setPlayer(ArrayList<Player> player){
-  this.player = player;
-}
-public ArrayList<Player> getPlayer(){
-  return player;
-}
+
+  public String getName(){
+    return name;
+  }
+
+  public void setPlayers(ArrayList<Player> players) { 
+    this.players = players;
+  }
+
+  public ArrayList<Player> getPlayer(){
+    return player;
+  }
 }
