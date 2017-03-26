1d4a92ba3399a64f7ddc9188781d566b4439b381
Kizenku <bakerjames1995@gmail.com>
2017-03-26 15:38:30 +0100

    Add required methods

--- src/main/java/com/github/poker/Dealer.java
+++ src/main/java/com/github/poker/Dealer.java
@@ -9,7 +9,8 @@ public class Dealer {
   private String name;
   private ArrayList<Player> players;
   
-  int startIdx;
+  int blindIdx;
+  
   Player subject;
 
   private Dealer() {}
@@ -32,12 +33,42 @@ public class Dealer {
     this.players = players;
   }
 
-  public void start() {
-    
+  public void startSession() {
+    startGame();
   }
 
-  public void init() {
-    startIdx=0;
+  public void startGame() {
+    initialBets();
+    flop();
+    turn();
+    river();
+    declareWinner();
   }
-  
+
+  public void initialBets() {
+    nextPlayer(subject);
+  }
+
+  public void flop() {
+
+  }
+
+  public void turn() {
+
+  }
+
+  public void river() {
+
+  }
+
+  public void declareWinner() {
+
+  }
+
+  public Player nextPlayer(Player current) {
+    if(current==null) {
+      return players.get(blindIdx);
+    }
+    return null;
+  }
 }
