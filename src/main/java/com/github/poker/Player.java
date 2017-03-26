f0348d45761e69d39cd40dce7b581461a2815140
tony <montananyto@hotmail.com>
2017-03-26 15:25:21 +0100

    Create Players

--- src/main/java/com/github/poker/Player.java
+++ src/main/java/com/github/poker/Player.java
@@ -4,54 +4,62 @@ import java.util.ArrayList;
 
 public class Player implements CardHolder {
 
-  public String name;
-  public int age;
-  public ArrayList<Card> hand;
+public String name;
+public int age;
+public ArrayList<Card> hand;
+public int chips;
 
-  public Player (String name, int age){
-    this.name = name;
-    this.age = age;
-    this.hand = new ArrayList<Card>();
+public Player (String name, int age,int chips){
+        this.name = name;
+        this.age = age;
+        this.hand = new ArrayList<Card>();
+        this.chips = chips;
 
-    //Initial hand
-    Deck.getInstance().giveCard(this,2);
+        //Initial hand
+        Deck.getInstance().giveCard(this,2);
 
-    System.out.println("I have...");
-    for(int i=0;i<2;i++) {
-      System.out.println(hand.get(i));
-    }
-  }
+        System.out.println("I have...");
+        for(int i=0; i<2; i++) {
+                System.out.println(hand.get(i));
+        }
+}
 
-  public void receiveCard(Card newCard) {
-    hand.add(newCard);
-  }
+public void receiveCard(Card newCard) {
+        hand.add(newCard);
+}
 
-  public void setName(String name){
-    this.name = name;
-  }
+public void setName(String name){
+        this.name = name;
+}
 
-  public String getName(){
-    return name;
-  }
+public String getName(){
+        return name;
+}
 
-  public void setAge(int age){
-    this.age = age;
-  }
+public void setAge(int age){
+        this.age = age;
+}
 
-  public int getAge(){
-    return age;
-  }
+public int getAge(){
+        return age;
+}
 
-  public void setHand(ArrayList<Card> hand){
-    this.hand = hand;
-  }
+public void setHand(ArrayList<Card> hand){
+        this.hand = hand;
+}
 
-  public ArrayList<Card> getHand(){
-    return hand;
-  }
+public ArrayList<Card> getHand(){
+        return hand;
+}
+public void setChips(int chips){
+        this.chips = chips;
+}
+public int getChips(){
+        return chips;
+}
 
-  public void printHand() {
-    for(Card aCard : hand)
-      System.out.print(aCard + "\n");
-  }
+public void printHand() {
+        for(Card aCard : hand)
+                System.out.print(aCard + "\n");
+}
 }
--- src/main/java/com/github/poker/StartGame.java
+++ src/main/java/com/github/poker/StartGame.java
@@ -6,24 +6,122 @@ import java.util.ArrayList;
 
 public class StartGame
 {
-    public static void main(String[] args)
-    {
+static int chips;
+static ArrayList<Player> players;
+static Scanner scan;
+static int Chips;
+static Dealer dealer;
+
+public static void main(String[] args)
+{
         System.out.println("Starting Game...");
 
-        Dealer dealer = Dealer.getInstance();
-        dealer.setName("dealer");
+        dealer = Dealer.getInstance();
+        dealer.setName("Dealer");
 
         Board board = Board.getInstance();
 
+        scan = new Scanner(System.in);
 
-        System.out.println("How many players ?");
-        Scanner scan = new Scanner(System.in);
-        int numOfPlayer = scan.nextInt();
+        players = new ArrayList<Player>();
 
-        Player player = new Player ("tony",18);
+        int numberOfPlayers = reqPlayerNumber();
 
-        ArrayList<Card> currentPlayerHand = new ArrayList<Card>();
-        currentPlayerHand = player.getHand();
+        int chips = reqChips();
 
-    }
+        do {
+                String name = reqPlayerName();
+                int age = reqPlayerAge();
+                Player player = new Player(name,age,chips);
+                players.add(player);
+                setPlayers();
+                numberOfPlayers--;
+
+        } while ( numberOfPlayers > 0 );
+
+
+}
+
+private static int reqPlayerNumber() {
+        boolean isNumber = false;
+        boolean aboveLimit = true;
+        String input;
+        int amount=0;
+        do {
+                System.out.println("How many Players (max. 5)");
+                input = scan.nextLine();
+
+                isNumber = isNumeric(input);
+                if(isNumber) {
+                        amount = Integer.parseInt(input);
+                        aboveLimit = amount > 5;
+                        if(aboveLimit) { System.out.println("Above maximum."); }
+                } else { System.out.println("Not a number."); }
+
+        } while(!isNumber || aboveLimit);
+
+        return amount;
+}
+
+private static int reqChips(){
+        System.out.println("How much do you want to give to each player");
+        chips = scan.nextInt();
+        scan.nextLine();
+
+        return chips;
+}
+
+private static void setPlayers(){
+        dealer.setPlayers(players);
+}
+
+private static String reqPlayerName() {
+        boolean nameInUse = true;
+        String input;
+        do {
+                System.out.println("Name of the player");
+                input = scan.nextLine();
+                nameInUse=playerExists(input);
+                if(nameInUse) { System.out.println("Name is already used."); }
+        } while(nameInUse);
+
+        return input;
+}
+
+private static int reqPlayerAge() {
+        int age = 0;
+        boolean tooYoung = true;
+        boolean isNumber = false;
+        String input;
+        do {
+                System.out.println("Age of the player");
+                input = scan.nextLine();
+
+                isNumber = isNumeric(input);
+                if(isNumber)  {
+                        age = Integer.parseInt(input);
+                        tooYoung = age < 18;
+                        if(tooYoung) { System.out.println("Too young."); }
+                } else { System.out.println("Not a number."); }
+
+        } while(tooYoung || !isNumber);
+
+        return age;
+}
+private static boolean isNumeric(String input) {
+        try {
+                Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+                return false;
+        }
+        return true;
+}
+private static boolean playerExists(String newName) {
+        for(Player aPlayer : players)
+                if(aPlayer.getName().equals(newName))
+                        return true;
+
+        return false;
+}
+
 }
