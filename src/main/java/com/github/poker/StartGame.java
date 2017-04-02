a97cea5b261b4cd52d05a724224475da130d9f9a
tony <montananyto@hotmail.com>
2017-04-02 13:34:02 +0100

    startgame updated

--- src/main/java/com/github/poker/StartGame.java
+++ src/main/java/com/github/poker/StartGame.java
@@ -8,33 +8,33 @@ public class StartGame {
 static ArrayList<Player> players;
 static Dealer dealer;
 
-        public static void main(String[] args) {
-                System.out.println("Starting Game...");
+public static void main(String[] args) {
+        System.out.println("Starting Game...");
 
-                dealer = Dealer.getInstance();
-                dealer.setName("Dealer");
+        dealer = Dealer.getInstance();
+        dealer.setName("Dealer");
 
-                Board board = Board.getInstance();
+        Board board = Board.getInstance();
 
-                players = new ArrayList<Player>();
+        players = new ArrayList<Player>();
 
-                int numberOfPlayers = Validator.reqPlayerNumber();
+        int numberOfPlayers = Validator.reqPlayerNumber();
 
-                int chips = Validator.reqChips();
+        int chips = Validator.reqChips();
 
-                do {
-                        String name = Validator.reqPlayerName(players);
-                        int age = Validator.reqPlayerAge();
-                        Player player = new Player(name,age,chips);
-                        players.add(player);
-                        setPlayers();
-                        numberOfPlayers--;
+        do {
+                String name = Validator.reqPlayerName(players);
+                int age = Validator.reqPlayerAge();
+                Player player = new Player(name,age,chips);
+                players.add(player);
+                setPlayers();
+                numberOfPlayers--;
 
-                } while ( numberOfPlayers > 0 );
-        }
+        } while ( numberOfPlayers > 0 );
+}
 
-        private static void setPlayers(){
-                dealer.setPlayers(players);
-        }
+private static void setPlayers(){
+        dealer.setPlayers(players);
+}
 
 }
